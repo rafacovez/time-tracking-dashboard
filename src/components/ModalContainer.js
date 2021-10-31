87dfb8a58d1b6972dd4fc853e5616c7c544c45f4
Rafa <adanescollante@gmail.com>
2021-10-31 14:38:02 -0400

    updates

--- src/components/ModalContainer.js
+++ src/components/ModalContainer.js
@@ -4,6 +4,30 @@ import iconOptions from "../images/icon-ellipsis.svg"
 import stats from "../data.json"
 
 function ModalContainer() {
+    const pickTimeframeDaily = () => {
+        document.getElementById("daily").classList.add("active")
+        if (document.getElementById("weekly").classList[1] === "active") {
+            document.getElementById("weekly").classList.remove("active")
+        } else if (document.getElementById("monthly").classList[1] === "active") {
+            document.getElementById("monthly").classList.remove("active")
+        }
+    }
+    const pickTimeframeWeekly = () => {
+        document.getElementById("weekly").classList.add("active")
+        if (document.getElementById("daily").classList[1] === "active") {
+            document.getElementById("daily").classList.remove("active")
+        } else if (document.getElementById("monthly").classList[1] === "active") {
+            document.getElementById("monthly").classList.remove("active")
+        }
+    }
+    const pickTimeframeMonthly = () => {
+        document.getElementById("monthly").classList.add("active")
+        if (document.getElementById("daily").classList[1] === "active") {
+            document.getElementById("daily").classList.remove("active")
+        } else if (document.getElementById("weekly").classList[1] === "active") {
+            document.getElementById("weekly").classList.remove("active")
+        }
+    }
     return (
         <div className="ModalContainer">
             <div className="profile">
@@ -17,9 +41,9 @@ function ModalContainer() {
                     </div>
                 </div>
                 <div className="profile-footer">
-                    <button className="profile-footer-btn">Daily</button>
-                    <button className="profile-footer-btn">Weekly</button>
-                    <button className="profile-footer-btn">Monthly</button>
+                    <button id="daily" onClick={pickTimeframeDaily} className="profile-footer-btn active">Daily</button>
+                    <button id="weekly" onClick={pickTimeframeWeekly} className="profile-footer-btn">Weekly</button>
+                    <button id="monthly" onClick={pickTimeframeMonthly} className="profile-footer-btn">Monthly</button>
                 </div>
             </div>
             {stats.map(stat => {
@@ -34,8 +58,8 @@ function ModalContainer() {
                                 <img className="modal-stat-footer-top-options" src={iconOptions} alt="icon options"/>
                             </div>
                             <div className="modal-stat-footer-bottom center">
-                                <span className="modal-stat-footer-bottom-time">{stat.timeframes.daily.current}hrs</span>
-                                <span className="modal-stat-footer-bottom-last">Last week - {stat.timeframes.daily.previous}hrs</span>
+                                <span id="currTime" className="modal-stat-footer-bottom-time">{stat.timeframes.daily.current}hrs</span>
+                                <span id="prevTime" className="modal-stat-footer-bottom-last">Last week - {stat.timeframes.daily.previous}hrs</span>
                             </div>
                         </div>
                     </div>
